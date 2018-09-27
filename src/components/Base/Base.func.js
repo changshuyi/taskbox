2217f58a1788b100427ebe19100e5930e58cadd4
shuyi <sueeloveblack@gmail.com>
2018-09-27 15:09:43 +0800

    commit change

--- src/components/Base/Base.func.js
+++ src/components/Base/Base.func.js
@@ -1,7 +1,6 @@
 import { action } from '@storybook/addon-actions';
 
 const actions = {
-  onTrafficClick: action('onTrafficClick'),
   onRightBlockSwitch: action('onRightBlockSwitch'),
   onPinTask: action('onPinTask'),
   onArchiveTask: action('onArchiveTask'),
